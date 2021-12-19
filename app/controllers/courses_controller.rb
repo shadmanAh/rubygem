495a4c206c0eb7134752319ed38b54d846817d83
shadmanAh <shadman061@gmail.com>
2021-12-19 21:41:37 +0330

    collections, scopes, ransack, pagy for courses and enrollments

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,29 +10,49 @@ class CoursesController < ApplicationController
     #   # @q = Course.ransack(params[:q])
     #   # @courses = @q.result.includes(:user)
     # end
+    
+    @ransack_path = courses_path
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
     # @courses = @ransack_courses.result.includes(:user)
 
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
   end
 
-  # GET /courses/1 or /courses/1.json
+  def purchased
+    @ransack_path = purchased_courses_path
+    @ransack_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    render 'index'
+  end
+
+  def pending_review
+    @ransack_path = pending_review_courses_path
+    @ransack_courses = Course.joins(:enrollments).merge(Enrollment.pending_review.where(user: current_user)).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    render 'index'
+  end
+
+  def created
+    @ransack_path = created_courses_path
+    @ransack_courses = Course.where(user: current_user).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    render 'index'
+  end
+
   def show
     @lessons = @course.lessons
   end
 
-  # GET /courses/new
+
   def new
     @course = Course.new
     authorize @course
   end
 
-  # GET /courses/1/edit
   def edit
     authorize @course
   end
 
-  # POST /courses or /courses.json
   def create
     @course = Course.new(course_params)
     authorize @course
@@ -49,7 +69,6 @@ class CoursesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /courses/1 or /courses/1.json
   def update
     authorize @course
     respond_to do |format|
@@ -63,7 +82,7 @@ class CoursesController < ApplicationController
     end
   end
 
-  # DELETE /courses/1 or /courses/1.json
+
   def destroy
     authorize @course 
     @course.destroy
@@ -74,12 +93,11 @@ class CoursesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_course
       @course = Course.friendly.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def course_params
       params.require(:course).permit(:title, :description, :short_description, :price, :language, :level)
     end
